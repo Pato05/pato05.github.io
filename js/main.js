5ee17fecda3fbaf473802c8dfe2afd0a908b9dd7
Pato05 <lo.gu2005@hotmail.it>
2019-05-13 22:02:20 +0200

    lol

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 "use strict";
 $(document).ready(function() {
   $(".sidenav").sidenav();
+  $(".modal").modal();
   setTimeout(function() {
     $("#loader").css("display", "none");
     $("main").fadeIn();
-    $(".modal").modal();
   }, 0);
 });
