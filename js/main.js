678dabe190195053910498193653bef9046754d7
Pato05 <admin@pato05mc.tk>
2020-01-24 17:07:53 +0100

    Sidenav now will be builded automatically.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 "use strict";
 $(document).ready(function() {
-  $(".sidenav").sidenav();
   $(".modal").modal();
+  buildSidebar()
   setTimeout(function() {
     $("#loader").css("display", "none");
     $("main").fadeIn();
   }, 0);
 });
+
+function buildSidebar() {
+  let sidebarItems = [
+    {'name':'Home', 'icon':'home', 'href':'/'},
+    {'name':'Easy Telegram Bot', 'icon':'sms', 'href':'/tg-js-bot'},
+    {'name':'PatoBot', 'icon':'code', 'href':'/PatoBot'},
+    {'name':'AdbFastbootInstaller', 'icon':'adb', 'href':'/AdbFastbootInstaller'}
+  ];
+  let sidebar = $('<ul>').addClass('sidenav sidenav-fixed').prop('id', 'sidebar-slide');
+  for(let item of sidebarItems) {
+    let newItem = $('<li>');
+    if(document.location.pathname == item['href']) newItem.addClass('active');
+    newItem.append($('<a>').addClass('waves-effect').prop('href', item['href']).append($('<i>').addClass('material-icons left').html(item['icon'])).append(document.createTextNode(item['name'])));
+    sidebar.append(newItem);
+  }
+  sidebar.sidenav();
+  $('body').append(sidebar)
+  return true
+}
